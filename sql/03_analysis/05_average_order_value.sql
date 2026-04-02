8fde7090d53b2d4517b6c7e64f4864e0db670118
andreasdads25 <andreasdads25@gmail.com>
2026-04-02 06:42:09 +0300

    Update 05_average_order_value.sql

--- sql/03_analysis/05_average_order_value.sql
+++ sql/03_analysis/05_average_order_value.sql
@@ -1,1 +1,11 @@
+/* ============================================================================
+   Query 05: Average Order Value (AOV)
+   Business Purpose:
+       Measure average revenue per order.
+   Technical Notes:
+       Total revenue / number of unique orders.
+   ============================================================================ */
 
+SELECT 
+    SUM(amount) / COUNT(*) AS avg_order_value
+FROM sales_clean;
